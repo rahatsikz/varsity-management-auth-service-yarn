55660f684a14a05250984fdf30c7fcba875e4992
rahatsikz <rascal6271@gmail.com>
2023-06-07 13:06:00 +0600

    [DONE] ~ initial project setup done

---
+++ README.md
@@ -0,0 +1,1 @@
+# varsity-management-auth-service-yarn
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, json } from "express";
+import cors from "cors";
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+export default app;
