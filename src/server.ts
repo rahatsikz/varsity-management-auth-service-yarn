3e432304484a3942218407b47b40dd1eead55c10
rahatsikz <rascal6271@gmail.com>
2023-06-07 21:19:05 +0600

    [DONE] ~ Eslint, prettier, husky,lint stage setup

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "../src/config/index";
+import app from './app'
+import config from '../src/config/index'
+import mongoose from 'mongoose'
 
 async function mainFunc() {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url as string)
+    console.log('🔋 Database Connected')
+
     app.listen(config.port, () => {
-      console.log(`Management app listening on port ${config.port}`);
-    });
+      console.log(`Management app listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("error found", error);
+    console.log('error found', error)
   }
 }
 
-mainFunc();
+mainFunc()
